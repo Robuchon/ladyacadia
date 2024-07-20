b63d463e3b62e81325052864cdf97849b90ae238
Robuchon Hugues <robuchon.hugues@orange.fr>
2024-07-20 23:37:47 +0200

    avancement de la page galerie

--- src/pages/Galerie.vue
+++ src/pages/Galerie.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="mx-auto mt-20 flex w-[80%] flex-col gap-4 md:flex-row">
-    <div name="gauche" class="space-y-2">
+    <div name="gauche" class="space-y-1">
       <div v-for="(image, index) in galerie" class="flex">
-        <div
-          v-if="index % 2 === 0"
-          class="relative my-auto"
-          @click="(open = true), (imageTarget = image)"
-        >
+        <div v-if="index % 3 === 0" class="relative my-auto" @click="(open = true), (imageTarget = image)">
           <div class="image-container">
             <h1
-              class="overlay  z-10 text-center text-4xl font-semibold text-Green bg-white rounded-full px-4 bg-opacity-90"
-            >
+              class="overlay  z-10 text-center text-4xl font-semibold text-Green bg-white rounded-full px-4 py-2 bg-opacity-90">
               &gt; {{ image.title }} &lt;
             </h1>
             <img class="image" :src="image.src" alt="" />
@@ -18,17 +13,25 @@
         </div>
       </div>
     </div>
-    <div name="droite" class="-mt-2 space-y-2">
+    <div name="centre" class="-mt-1 space-y-1">
       <div v-for="(image, index) in galerie" class="flex">
-        <div
-          v-if="index % 2 !== 0"
-          class="relative my-auto"
-          @click="(open = true), (imageTarget = image)"
-        >
+        <div v-if="index % 3 === 1" class="relative my-auto" @click="(open = true), (imageTarget = image)">
           <div class="image-container">
             <h1
-              class="overlay  z-10 text-center text-4xl font-semibold text-Green bg-white rounded-full px-4 bg-opacity-90"
-            >
+              class="overlay  z-10 text-center text-4xl font-semibold text-Green bg-white rounded-full px-4 py-2 bg-opacity-90">
+              &gt; {{ image.title }} &lt;
+            </h1>
+            <img class="image" :src="image.src" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div name="droite" class="-mt-2 space-y-1">
+      <div v-for="(image, index) in galerie" class="flex">
+        <div v-if="index % 3 === 2" class="relative my-auto" @click="(open = true), (imageTarget = image)">
+          <div class="image-container">
+            <h1
+              class="overlay  z-10 text-center text-4xl font-semibold text-Green bg-white rounded-full px-4 py-2 bg-opacity-90">
               &gt; {{ image.title }} &lt;
             </h1>
             <img class="image" :src="image.src" alt="" />
@@ -37,29 +40,16 @@
       </div>
     </div>
     <TransitionRoot as="template" :show="open">
-      <TransitionChild
-        as="template"
-        enter="ease-out duration-500"
-        enter-from="opacity-0 translate-y-8 sm:translate-y-0 "
-        enter-to="opacity-100 translate-y-0 sm:scale-100"
-        leave="ease-in duration-500"
-        leave-from="opacity-100 translate-y-0 sm:scale-100"
-        leave-to="opacity-0 translate-y-8 sm:translate-y-0"
-      >
-        <div
-          class="fixed top-0 left-0 right-0 bottom-0 z-20 flex overflow-hidden bg-white"
-        >
+      <TransitionChild as="template" enter="ease-out duration-500"
+        enter-from="opacity-0 translate-y-8 sm:translate-y-0 " enter-to="opacity-100 translate-y-0 sm:scale-100"
+        leave="ease-in duration-500" leave-from="opacity-100 translate-y-0 sm:scale-100"
+        leave-to="opacity-0 translate-y-8 sm:translate-y-0">
+        <div class="fixed top-0 left-0 right-0 bottom-0 z-20 flex overflow-hidden bg-white">
           <div class="relative flex h-full w-full flex-col items-center lg:flex-row justify-evenly">
             <XMarkIcon
               class="absolute top-4 right-4 m-1 h-10 w-10 rounded-full p-1 text-gray-600 hover:border-2 hover:border-Anthracite hover:text-Anthracite"
-              aria-hidden="true"
-              @click="open = false"
-            />
-            <img
-              :src="imageTarget.src"
-              alt=""
-              class="mx-auto lg:mx-0 max-h-[80%] max-w-[90%]"
-            />
+              aria-hidden="true" @click="open = false, imageTarget = ''" />
+            <img :src="imageTarget.src" alt="" class="mx-auto lg:mx-0 max-h-[80%] max-w-[90%]" />
             <div class="flex flex-col justify-center text-center w-[90%] md:max-w-[25%] min-h-[20%]">
               <h1 class="">&gt; {{ imageTarget?.title }} &lt;</h1>
               <p class="">{{ imageTarget?.txt }}</p>
